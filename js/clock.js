46c49b9bdbf28482d66916e718afbfeb9ae9f719
yesurooo <56687700+yesurooo@users.noreply.github.com>
2020-03-24 22:25:14 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,11 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+  clockTitle = clockContainer.querySelector(".js-clock__title");
 
 function getTime() {
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
